a305abffc27a51ac662c152ad878245501ecebcb
Shouvik Roy <shouvikshikhar@gmail.com>
2021-06-02 00:34:39 -0400

    modified index.html, media query, used javascript to make a functional hamburger menu for navigation

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+const hamburger = document.querySelector('.hamburger');
+const homeUl = document.querySelector('.home-ul');
+
+
+hamburger.addEventListener('click', function() {
+    homeUl.classList.toggle('show');
+   
+});
+
+
+// const test = document.querySelector('.banner-text');
+
+// test.addEventListener('click', function() {
+
+// })
